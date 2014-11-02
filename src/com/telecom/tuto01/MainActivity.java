af3c49fda2dd52437be935a48a0947c42fca562c
ChampF <champ.frederic@gmail.com>
2014-11-02 21:11:34 +0100

    Updated main layout

--- src/com/telecom/tuto01/MainActivity.java
+++ src/com/telecom/tuto01/MainActivity.java
@@ -2,12 +2,15 @@ package com.telecom.tuto01;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.Window;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
+
+    private TextView m_text = null;
+    private EditText m_editText = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -16,6 +19,10 @@ public class MainActivity extends Activity {
         this.requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         this.setContentView(R.layout.main_activity);
         this.getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.title);
+
+        // get view item
+        m_text = (TextView) findViewById(R.id.textId);
+        m_editText = (EditText) findViewById(R.id.editTextId);
     }
 
     @Override
